093bc648d3ec2a99cd3c6ca228154361c1aa92b1
Roland Bali <roland@spagettikod.se>
2023-08-29 07:12:55 +0200

    new command `db list` Fixes #83

---
+++ cmd/dblist.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spagettikod/vsix/database"
+	"github.com/spf13/cobra"
+)
+
+var quiet bool
+
+func init() {
+	dbListCmd.Flags().BoolVarP(&quiet, "quiet", "q", false, "display result in short format, usually the item identifier")
+	dbCmd.AddCommand(dbListCmd)
+}
+
+var dbListCmd = &cobra.Command{
+	Use:                   "list",
+	Aliases:               []string{"ls"},
+	Short:                 "List extensions in the database",
+	Long:                  `List extensions in the database.`,
+	Example:               "  $ vsix db list",
+	DisableFlagsInUseLine: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		logger := log.With().Str("path", dbPath).Logger()
+		start := time.Now()
+		db, err := database.OpenFs(dbPath, false)
+		if err != nil {
+			log.Fatal().Err(err).Str("database_root", dbPath).Msg("could not open database")
+		}
+		exts := db.List(true)
+		for _, ext := range exts {
+			fmt.Printf("%s\n", ext.UniqueID())
+		}
+		logger.Debug().Msgf("total time for list %.3fs", time.Since(start).Seconds())
+	},
+}
